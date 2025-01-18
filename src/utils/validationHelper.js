5b2e074b711bea9cb49c9dce30c27fc79091a7f5
Yevhenii <yevhenii.katsimon@gmail.com>
2025-01-18 23:21:37 +0200

    Refactor validation helper to use ES modules; update imports and exports for validation functions

--- src/utils/validationHelper.js
+++ src/utils/validationHelper.js
@@ -1,54 +1,49 @@
+import { createError } from '#utils/errorsHelper.js'
+import { errors } from '#consts/errors.js'
+
 const {
   FIELD_IS_NOT_DEFINED,
   FIELD_IS_NOT_OF_PROPER_TYPE,
   FIELD_IS_NOT_OF_PROPER_LENGTH,
   FIELD_IS_NOT_OF_PROPER_FORMAT,
   FIELD_IS_NOT_OF_PROPER_ENUM_VALUE
-} = require('~/consts/errors')
-const { createError } = require('./errorsHelper')
+} = errors
 
-const validateRequired = (schemaFieldKey, required, field) => {
+export const validateRequired = (schemaFieldKey, required, field) => {
   if (required && !field) {
     throw createError(422, FIELD_IS_NOT_DEFINED(schemaFieldKey))
   }
 }
 
-const validateType = (schemaFieldKey, type, field) => {
+export const validateType = (schemaFieldKey, type, field) => {
   if (type != typeof field) {
     throw createError(422, FIELD_IS_NOT_OF_PROPER_TYPE(schemaFieldKey, type))
   }
 }
 
-const validateLength = (schemaFieldKey, length, field) => {
+export const validateLength = (schemaFieldKey, length, field) => {
   if (field.length < length.min || field.length > length.max) {
     throw createError(422, FIELD_IS_NOT_OF_PROPER_LENGTH(schemaFieldKey, length))
   }
 }
 
-const validateRegex = (schemaFieldKey, regex, field) => {
+export const validateRegex = (schemaFieldKey, regex, field) => {
   if (!regex.test(field)) {
     throw createError(422, FIELD_IS_NOT_OF_PROPER_FORMAT(schemaFieldKey))
   }
 }
 
-const validateEnum = (schemaFieldKey, enumSet, field) => {
+export const validateEnum = (schemaFieldKey, enumSet, field) => {
   const isEnumValue = enumSet.some((value) => value === field)
   if (!isEnumValue) {
     throw createError(422, FIELD_IS_NOT_OF_PROPER_ENUM_VALUE(schemaFieldKey, enumSet))
   }
 }
 
-const validateFunc = {
+export const validateFunc = {
   required: validateRequired,
   type: validateType,
   length: validateLength,
   regex: validateRegex,
   enum: validateEnum
-}
-
-module.exports = {
-  validateRequired,
-  validateType,
-  validateLength,
-  validateFunc
 }
