cb749c7bc69d6f6633642cfc1b4fb96e73d58f35
Yevgenii <yevhenii.katsimon@gmail.com>
2025-01-23 01:46:21 +0200

    feat: add Swagger documentation for logout API endpoint

---
+++ src/docs/swagger/logout.swagger.js
@@ -0,0 +1,17 @@
+/**
+ * @swagger
+ * tags:
+ *   - name: Auth
+ *     description: Authentication user
+ */
+
+/**
+ * @swagger
+ * /auth/logout:
+ *   post:
+ *     summary: Logout user
+ *     tags: [Auth]
+ *     responses:
+ *       204:
+ *         description: Successfully logged out
+ */
