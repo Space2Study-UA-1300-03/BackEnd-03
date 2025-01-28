42a57ab327c677806c895552ccddd9fa04449b17
Yevhenii <yevhenii.katsimon@gmail.com>
2025-01-28 17:02:21 +0200

    refactor: remove unused test helpers and update Jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,32 +1,55 @@
-// export default {
-//   roots: ['<rootDir>/src/test'],
-//   moduleNameMapper: {
-//     '^~/(.*)$': '<rootDir>/src/$1'
-//   },
-//   verbose: true,
-//   testEnvironment: 'node',
-//   collectCoverage: true,
-//   collectCoverageFrom: [
-//     '!<rootDir>/node_modules/*',
-//     '!<rootDir>/src/test/**/*',
-//     '!<rootDir>/src/consts/*',
-//     '!<rootDir>/src/configs/*',
-//     '!<rootDir>/docs/*',
-//     '!<rootDir>/src/emails/*',
-//     '!<rootDir>/*.json',
-//     '!<rootDir>/*.yaml'
-//   ],
-//   coverageThreshold: {
-//     global: {
-//       branches: 70,
-//       functions: 70,
-//       lines: 70,
-//       statements: 70
-//     }
-//   },
-//   coverageReporters: ['html', 'lcov'],
-//   coverageDirectory: '<rootDir>/src/test/coverage',
-//   testTimeout: 12000,
-//   testMatch: ['<rootDir>/src/test/integration/**/*.spec.js', '<rootDir>/src/test/unit/**/*.spec.js'],
-//   testResultsProcessor: 'jest-sonar-reporter'
-// }
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+/** @type {import('jest').Config} */
+const config = {
+  roots: ['<rootDir>/src/test'],
+  moduleNameMapper: {
+    '^~/(.*)$': '<rootDir>/src/$1',
+    '^#src/(.*)$': '<rootDir>/src/$1',
+    '^#configs/(.*)$': '<rootDir>/src/configs/$1',
+    '^#consts/(.*)$': '<rootDir>/src/consts/$1',
+    '^#utils/(.*)$': '<rootDir>/src/utils/$1',
+    '^#controllers/(.*)$': '<rootDir>/src/controllers/$1',
+    '^#middlewares/(.*)$': '<rootDir>/src/middlewares/$1',
+    '^#services/(.*)$': '<rootDir>/src/services/$1',
+    '^#cron-jobs/(.*)$': '<rootDir>/src/cron-jobs/$1',
+    '^#seed/(.*)$': '<rootDir>/src/seed/$1',
+    '^#routes/(.*)$': '<rootDir>/src/routes/$1',
+    '^#models/(.*)$': '<rootDir>/src/models/$1',
+    '^#validation/(.*)$': '<rootDir>/src/validation/$1'
+  },
+  verbose: true,
+  collectCoverage: true,
+  testEnvironment: 'node',
+  coverageReporters: ['html', 'lcov'],
+  coverageDirectory: '<rootDir>/src/test/coverage',
+  testTimeout: 12000,
+
+  collectCoverageFrom: [
+    '!<rootDir>/node_modules/*',
+    '!<rootDir>/src/test/**/*',
+    '!<rootDir>/src/consts/*',
+    '!<rootDir>/src/configs/*',
+    '!<rootDir>/docs/*',
+    '!<rootDir>/src/emails/*',
+    '!<rootDir>/*.json',
+    '!<rootDir>/*.yaml'
+  ],
+
+  coverageThreshold: {
+    global: {
+      statements: 70,
+      functions: 70,
+      branches: 70,
+      lines: 70
+    }
+  },
+
+  testMatch: ['<rootDir>/src/test/integration/**/*.test.js', '<rootDir>/src/test/unit/**/*.test.js'],
+  testResultsProcessor: 'jest-sonar-reporter'
+}
+
+export default config
---
+++ src/test/unit/sum.test.js
@@ -0,0 +1,21 @@
+/**
+ * @description Checking if jest unit tests are working
+ */
+
+const sum = (a, b) => a + b
+
+import { describe, expect, xit, it } from '@jest/globals'
+
+describe('sum function', () => {
+  it('return 5', () => {
+    const result = sum(2, 3)
+    expect(result).toBe(5)
+  })
+
+  xit('not return null, undefined', () => {
+    const result = sum(2, 3)
+    expect(result).not.toEqual(0)
+    expect(result).not.toBeNull()
+    expect(result).not.toBeUndefined()
+  })
+})
