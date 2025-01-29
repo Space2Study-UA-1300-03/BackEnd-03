e2913b0ae0acf5809022f34aec843cc0195d699b
Yevhenii <yevhenii.katsimon@gmail.com>
2025-01-29 13:48:43 +0200

    feat: enhance email sending functionality with validation and error handling; update route parameters for consistency

--- src/controllers/email.js
+++ src/controllers/email.js
@@ -1,10 +1,21 @@
+import { createError } from '#utils/errorsHelper.js'
 import { emailService } from '#services/email.js'
+import { errors } from '#consts/errors.js'
+import { logger } from '#logger/logger.js'
 
 export const sendEmail = async (req, res) => {
   const { email, subject, text } = req.body
   const lang = req.lang
 
-  await emailService.sendEmail(email, subject, lang, text)
+  if (!email || !subject || !text) {
+    return res.status(400).json({ error: 'Missing required fields' })
+  }
 
-  res.status(204).end()
+  try {
+    await emailService.sendEmail(email, subject, lang, text)
+    res.status(204).end()
+  } catch (error) {
+    logger.error(error)
+    throw createError(400, errors.API_TOKEN_NOT_RETRIEVED)
+  }
 }
