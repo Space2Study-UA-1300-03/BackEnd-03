d9d7b242f19f6f12193ca8ff9d7ef45c2346724a
Yevgenii <126769681+uncle-Yevhenii@users.noreply.github.com>
2025-01-30 12:55:57 +0200

    Test/#121 write unit test for id validation middleware (#25)
    * refactor: remove husky pre-commit and pre-push hooks, clean up package dependencies
    
    * fix: update production start script and enable strict query for mongoose
    
    * feat: refactor code to use ES6 module syntax and add ping controller
    
    * refactor: remove unused test helpers and update Jest configuration
    
    * feat: add integration tests for ping controller and improve Jest configuration
    
    * fix: update Jest tests to use 'it' instead of 'xit' for proper execution
    
    * test: add unit tests for idValidation middleware to validate ID formats
    
    * fix: remove unnecessary comment in idValidation test

---
+++ src/test/unit/middlewares/idValidation.test.js
@@ -0,0 +1,56 @@
+import { jest, describe, it, expect, beforeEach } from '@jest/globals'
+import mongoose from 'mongoose'
+
+const mockCreateError = jest.fn((status, message) => {
+  const error = new Error(message)
+  error.status = status
+  error.code = 'INVALID_ID'
+  error.message = 'ID is invalid.'
+  throw error
+})
+
+await jest.unstable_mockModule('#utils/errorsHelper.js', () => ({
+  createError: mockCreateError
+}))
+
+const { idValidation } = await import('#middlewares/idValidation.js')
+const { errors } = await import('#consts/errors.js')
+
+const { INVALID_ID } = errors
+
+describe('idValidation Middleware', () => {
+  let req, res, next
+
+  beforeEach(() => {
+    req = {}
+    res = {}
+    next = jest.fn()
+    mockCreateError.mockClear()
+  })
+
+  it('should call next() if ID is valid', () => {
+    const validId = new mongoose.Types.ObjectId().toString()
+
+    expect(() => idValidation(req, res, next, validId)).not.toThrow()
+    expect(next).toHaveBeenCalled()
+    expect(mockCreateError).not.toHaveBeenCalled()
+  })
+
+  it('should throw an error if ID is invalid', () => {
+    const invalidId = 'invalid-id'
+
+    try {
+      idValidation(req, res, next, invalidId)
+    } catch (error) {
+      expect(error).toEqual(
+        expect.objectContaining({
+          message: 'ID is invalid.',
+          status: 400,
+          code: 'INVALID_ID'
+        })
+      )
+    }
+    expect(mockCreateError).toHaveBeenCalledWith(400, INVALID_ID)
+    expect(next).not.toHaveBeenCalled()
+  })
+})
