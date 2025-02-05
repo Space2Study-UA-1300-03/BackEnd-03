dc39481461834f3e8cb2dd6be59c46fabddc080c
Yevhenii <yevhenii.katsimon@gmail.com>
2025-02-05 12:35:41 +0200

    feat: add ID validation middleware for category routes

--- src/routes/category.js
+++ src/routes/category.js
@@ -1,8 +1,12 @@
 import { getAllCategories, getCategoryById } from '#controllers/category.js'
 import { asyncWrapper } from '#middlewares/asyncWrapper.js'
+import { idValidation } from '#middlewares/idValidation.js'
+
 import express from 'express'
 
 export const router = express.Router()
+
+router.param('id', idValidation)
 
 router.get('/', asyncWrapper(getAllCategories))
 router.get('/:id', asyncWrapper(getCategoryById))
