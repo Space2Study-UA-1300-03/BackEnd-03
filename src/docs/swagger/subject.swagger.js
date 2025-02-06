7a79d76800b3c7f1659de0c1ee7b753fa7523fc2
Yevhenii <yevhenii.katsimon@gmail.com>
2025-02-06 12:30:33 +0200

    feature create swagger docs in subject GET route

--- src/docs/swagger/subject.swagger.js
+++ src/docs/swagger/subject.swagger.js
@@ -28,3 +28,49 @@
  *           format: date-time
  *           description: Timestamp of last subject update
  */
+
+/**
+ * @swagger
+ * tags:
+ *   - name: Subjects
+ *     description: Subjects management operations
+ */
+
+/**
+ * @swagger
+ * /subjects:
+ *   get:
+ *     summary: Get all subjects
+ *     tags: [Subjects]
+ *     responses:
+ *       200:
+ *         description: List of all subjects
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   _id:
+ *                     type: string
+ *                     format: uuid
+ *                     example: "67a48d5c26fc9e228d5a67d9"
+ *                   subjectName:
+ *                     type: string
+ *                     description: Name of the subject (lowercase)
+ *                     example: "physics"
+ *                   categoryId:
+ *                     type: string
+ *                     format: uuid
+ *                     description: Reference to the category
+ *                     example: "67a48d5626fc9e228d5a67d6"
+ *                   createdAt:
+ *                     type: string
+ *                     format: date-time
+ *                     example: "2025-02-06T10:22:20.706Z"
+ *                   updatedAt:
+ *                     type: string
+ *                     format: date-time
+ *                     example: "2025-02-06T10:22:20.706Z"
+ */
