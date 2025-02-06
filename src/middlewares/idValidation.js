41363fde6c7760a9275020978b2b5f40dbafb96f
Yevhenii <yevhenii.katsimon@gmail.com>
2025-02-06 14:03:59 +0200

    fix: update import for error handling in ID validation middleware

--- src/middlewares/idValidation.js
+++ src/middlewares/idValidation.js
@@ -1,8 +1,8 @@
 import { createError } from '#utils/errorsHelper.js'
-import { errors } from '#consts/errors.js'
+import { error } from '#consts/validationError.js'
 import mongoose from 'mongoose'
 
-const { INVALID_ID } = errors
+const { INVALID_ID } = error
 
 export const idValidation = (_req, _res, next, id) => {
   if (!mongoose.Types.ObjectId.isValid(id)) throw createError(400, INVALID_ID)
