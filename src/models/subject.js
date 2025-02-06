2c68fd245842557be166884f0575b31d3147859a
Yevhenii <yevhenii.katsimon@gmail.com>
2025-02-06 09:56:04 +0200

    feat: add subject model with validation for name and category fields

---
+++ src/models/subject.js
@@ -0,0 +1,27 @@
+import { errors } from '#consts/errors.js'
+import { Schema, model } from 'mongoose'
+import { refs } from '#consts/models.js'
+
+const { FIELD_CANNOT_BE_EMPTY } = errors
+const { SUBJECT, CATEGORY } = refs
+
+const subjectSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, FIELD_CANNOT_BE_EMPTY('name')],
+      lowercase: true
+    },
+    category: {
+      type: Schema.Types.ObjectId,
+      ref: CATEGORY,
+      required: [true, FIELD_CANNOT_BE_EMPTY('category')]
+    }
+  },
+  {
+    timestamps: true,
+    versionKey: false
+  }
+)
+
+export default model(SUBJECT, subjectSchema)
