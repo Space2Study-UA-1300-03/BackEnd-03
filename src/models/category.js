cb7670a3d0ecccf8a1c3ef4e8eed816350462951
Yevhenii <yevhenii.katsimon@gmail.com>
2025-02-16 16:07:13 +0200

    Add offerInfo field to category schema for associating offers with categories

--- src/models/category.js
+++ src/models/category.js
@@ -5,7 +5,7 @@ import { refs } from '#consts/models.js'
 
 const { FIELD_CANNOT_BE_EMPTY } = errors
 const { icon, color } = appearances
-const { CATEGORY } = refs
+const { CATEGORY, OFFER } = refs
 
 const categorySchema = new Schema(
   {
@@ -18,6 +18,14 @@ const categorySchema = new Schema(
     appearance: {
       icon: { type: String, default: icon, lowercase: true },
       color: { type: String, default: color, lowercase: true }
+    },
+    offerInfo: {
+      type: [
+        {
+          offerId: { type: Schema.Types.ObjectId, ref: OFFER }
+        }
+      ],
+      default: []
     }
   },
   {
