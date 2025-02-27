09a1c900bdce757d9345495f0a2821437ec75a6f
Yevhenii <yevhenii.katsimon@gmail.com>
2025-02-27 13:35:44 +0200

    Fix: update environment file path for production configuration

--- src/configs/envSetup.js
+++ src/configs/envSetup.js
@@ -7,7 +7,7 @@ const __dirname = path.dirname(__filename)
 
 export const loadEnvConfig = () => {
   const NODE_ENV = process.env?.NODE_ENV || 'development'
-  const envFile = NODE_ENV === 'production' ? '.env.local' : '.env.test.local'
+  const envFile = NODE_ENV === 'production' ? '.env' : '.env.test.local'
   const envPath = path.join(__dirname, '../..', envFile)
   const result = dotenv.config({ path: envPath })
 
